6065ab3479013863175d1d544e77ba9516c4280c
KGood93 <goodreau.kath@gmail.com>
2020-01-07 21:24:14 -0600

    install uuid and write seed file for quotes

---
+++ seeds/seed.sparkle_quotes.sql
@@ -0,0 +1,17 @@
+INSERT INTO sparkle_quotes (quoteId, quote, author, used)
+    VALUES
+        ( '001' ,'Be proud of how hard you are trying', 'NA', '0'),
+        ( '002' ,'Appreciate where you are in your journey, even if its not where you want to be. Every season serves a purpose.', 'NA', '0'),
+        ( '003' ,'I speak my mind because it hurts to bite my tounge', 'Gone with the Wind', '0'),
+        ( '004' ,'Burdens are for shoulders strong enough to carry them', 'Gone with the Wind', '0'),
+        ( '005' ,'After all tomorrow is another day', 'Gone with the Wind', '0'),
+        ( '006' ,'Nothing has turned out as we expected. It never does. Lifes under no obligation to give us what we expect. We take what we get and are thankful its no worse than it is. ', 'Gone with the Wind', '0'),
+        ( '007' ,'With enough courage you can do without a reputation', 'NA', '0'),
+        ( '008' ,'The goal is not to be successful, the goal is to be valuable. Once youre valuable instead of chasing success it will attract itself to you', 'NA', '0'),
+        ( '009' ,'Bet on yourself', 'NA', '0'),
+        ( '010' ,'The only thing more expensive than education is ignorance.', 'Benjamin Franklin', '0'),
+        ( '011' ,'This is the beginning of anything you want.', 'NA', '0'),
+        ( '012' ,'Do not ever quit out of fear of rejection.', 'J.K. Rowling', '0'),
+        ( '013' ,'Read a lot. Reading really helps. Read anything you can get your hands on.', 'J.K. Rowling', '0'),
+        ( '014' ,'Anything is possible if youve got enough nerve.', 'J.K. Rowling', '0'),
+        ( '015' ,'The world is full of wonderful things you havent seen yet. Dont ever give up on the chance of seeing them.', 'J.K. Rowling', '0'),

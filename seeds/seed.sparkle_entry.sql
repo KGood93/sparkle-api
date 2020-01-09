d295fe9df33523ff279bd714aee50bdd7f9e33d6
KGood93 <goodreau.kath@gmail.com>
2020-01-08 22:09:14 -0600

    Seed Entry database

---
+++ seeds/seed.sparkle_entry.sql
@@ -0,0 +1,7 @@
+INSERT INTO sparkle_entry (entryId, name, content, journalId, quoteId)
+    VALUES
+        (101, "Entry 1", "Test Content 1", 100, 1),
+        (102, "Entry 2", "Test Content 2", 100, 2),
+        (103, "Entry 3", "Test Content 3", 100, 3),
+        (104, "Entry 4", "Test Content 4", 100, 4),
+        (105, "Entry 5", "Test Content 5", 100, 5)

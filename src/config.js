f79485bbffaa0ddcce6d624b376f77410668acac
KGood93 <goodreau.kath@gmail.com>
2020-02-04 20:54:48 -0600

    Add Client Origin

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Katy:test@localhost/sparkle-api',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY:process.env.JWT_EXPIRY || '200000s',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Katy:test@localhost/sparkle-api-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Katy:test@localhost/sparkle-api-test',
+    CLIENT_ORIGIN: 'https://sparkle-app.now.sh'
 }

012f1aa6e4066163ef78271e179af9aae9772a7c
KGood93 <goodreau.kath@gmail.com>
2020-03-20 22:53:14 -0500

    update config and remove unused code

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ const {NODE_ENV, CLIENT_ORIGIN} = require('./config')
 const entryRouter = require('./entry/entry-router')
 const journalRouter = require('./journal/journal-router')
 const quoteRouter = require('./quotes/quote-router')
-//const authRouter = require('./auth/auth-router')
-//const usersRouter = require('./users/users-router')
 
 const app = express()
 
@@ -20,41 +18,17 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+//app.use(cors())
 
-//app.use(
-//    cors({
-//        origin: CLIENT_ORIGIN
-//    })
-//);
-
-//const whiteList = ['https://sparkle-app.now.sh', 'https://sparkle-app-1bojitddx.now.sh', 'https://sparkle-app.goodreaukath.now.sh']
-
-//const corsOptions = {
-//    origin: function(origin, callback) {
-//        if(whiteList.indexOf(origin) !== -1) {
-//            callback(null,true)
-//        }
-//        else {
-//            callback(new Error('Not allowed by CORS'))
-//        }
-//    }
-//}
-
-//if(NODE_ENV === 'production') {
-//    app.use(cors(corsOptions))
-//}
-//else {
-//    app.use(cors())
-//}
-
-
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.use('/entry', entryRouter)
 app.use('/journal', journalRouter)
 app.use('/quote', quoteRouter)
-//app.use('/auth', authRouter)
-//app.use('/users', usersRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Katy:test@localhost/sparkle-api',
-    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    JWT_EXPIRY:process.env.JWT_EXPIRY || '200000s',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Katy:test@localhost/sparkle-api-test',
-    CLIENT_ORIGIN: ['https://sparkle-app.now.sh', 'https://sparkle-app-1bojitddx.now.sh', 'https://sparkle-app.goodreaukath.now.sh']
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    //JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    //JWT_EXPIRY:process.env.JWT_EXPIRY || '200000s',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+    CLIENT_ORIGIN: 'https://sparkle-app.now.sh'
 }
